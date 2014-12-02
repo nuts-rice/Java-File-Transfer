1fdb439ee6850c05d43a8da2fdfbd9df711ba491
nuts-rice <red44443@gmail.com>
2014-12-02 07:26:03 -0600

    Update serverProject.java

--- serverProject.java
+++ serverProject.java
@@ -1,46 +1,71 @@
+package fileSharing;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
-public class serverProject implements Runnable {
+public class server implements Runnable {
 	private Socket	socket = null;
-	private ServerSocket server = null;
+	private static ServerSocket server = null;
 	private Thread thread = null;
-	//datainput is coming into server
-	private DataInputStream streamIn = null;
-	private final Int PORT = 60534;
-	private final String handshakeStart = ("HI_THERE");
-	private final String handshakeAck = ("BACK_AT_YA");
-	private String clientUsername= null;
-	private String serverUsername = null;
+	
+	
+	private final static int PORT = 60534;
+	
+	
+	
+	
 	private final String badHandshake = ("ERROR BAD HANDSHAKE");
 	
-	public FileServer(int PORT)
+	public static void main(String[] args) throws Exception
 	{
-		try
-      { 
-	  System.out.println("Binding to port " + port + ", please wait  ...");
+		 System.out.println("Binding to port " + PORT + ", please wait  ...");	 
 	  //initializing server socket
-         server = new ServerSocket(port);  
+         ServerSocket serverListen = new ServerSocket(PORT);  
          System.out.println("Server started: " + server);
-         start();
-      }
-      catch(IOException ioe)
-      {  System.out.println(ioe); 
-		 System.out.println(badHandshake);
-		 
-      }
-   }
+         try {
+        	 while (true) {
+        		 System.out.println("Connection coming in from" + serverListen.getLocalPort());
+        	 
+        		 new serverThread(serverListen.accept().start();
+        	 }
+         }
+         finally {
+        	 //closes server socket
+        	 serverListen.close();
+         }
+         
+	}
+	
+//	thread works with setting up 
+//	initilization strings of client and server names
+//	and "hi there" and "back at ya" 
+// 	as well as datainput stream
+	
+	private static class serverThread extends Thread	{
+		private final String handshakeStart = ("HI_THERE");
+		private final String handshakeAck = ("BACK_AT_YA");
+		private String clientUsername;
+		private String serverUsername = null;
+		//datainput is coming into server
+		//can use it to take input 
+		private DataInputStream streamIn;
+		try	{
+			clientInput = new DataInputStream(serverListen.)
+		}
+		
+	}
+		
+	
+    
 		
 		
-		while (true)
-		{
-			System.out.println("Connection coming in from" + soc.getLocalPort());
-			Socket soc = server.accept(); //takes in clients 
-			System.out.println(
 			
-			try
-			{
-				String line = 
+
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
 			
+		}
