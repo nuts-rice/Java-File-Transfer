2552e36efa300eb6c76e06e51c3cd0d1cf9a2359
nuts-rice <red44443@gmail.com>
2014-12-05 07:02:13 -0600

    Update clientProject.java

--- clientProject.java
+++ clientProject.java
@@ -1,56 +1,93 @@
 package fileSharing;
+
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
-public class client2client
-{  private Socket socket              = null;
-   private DataInputStream  console   = null;
-   private DataOutputStream streamOut = null;
+public class client2client {
+	private Socket socket = null;
+	private DataInputStream streamIn = null;
+	private DataOutputStream streamOut = null;
+	private String fileName;
+	private final String handshakeStart = ("HI_THERE");
+	private final String handshakeAck = ("BACK_AT_YA");
+	private String badHandhake = ("ERROR BAD HANDSHAKE");
+	private final static int PORT = 60534;
+	private Scanner clientScanner = null;
+	private String clientName;
+	private String serverName;
 
-   public	client2client(String serverName, int serverPort)
-   {  System.out.println("Establishing connection. Please wait ...");
-      try
-      {  socket = new Socket(serverName, serverPort);
-         System.out.println("Connected: " + socket);
-         start();
-      }
-      catch(UnknownHostException uhe)
-      {  System.out.println("Host unknown: " + uhe.getMessage());
-      }
-      catch(IOException ioe)
-      {  System.out.println("Unexpected exception: " + ioe.getMessage());
-      }
-      String line = "";
-      while (!line.equals(".bye"))
-      {  try
-         {  line = console.readLine();
-            streamOut.writeUTF(line);
-            streamOut.flush();
-         }
-         catch(IOException ioe)
-         {  System.out.println("Sending error: " + ioe.getMessage());
-         }
-      }
-   }
-   public void start() throws IOException
-   {  console   = new DataInputStream(System.in);
-      streamOut = new DataOutputStream(socket.getOutputStream());
-   }
-   public void stop()
-   {  try
-      {  if (console   != null)  console.close();
-         if (streamOut != null)  streamOut.close();
-         if (socket    != null)  socket.close();
-      }
-      catch(IOException ioe)
-      {  System.out.println("Error closing ...");
-      }
-   }
-   public static void main(String args[])
-   {  client2client client = null;
-      if (args.length != 2)
-         System.out.println("Usage: java ChatClient host port");
-      else
-         client = new client2client(args[0], Integer.parseInt(args[1]));
-   }
+	public client2client(String clientName) {
+		System.out.println("Set client username");
+		clientScanner = new Scanner(System.in);
+		this.clientName = clientScanner.nextLine();
+		System.out.println("What is name of server you're connecting to?");
+		this.serverName = clientScanner.nextLine();
+		System.out.println("Establishing connection. Please wait ...");
+		try {
+			socket = new Socket(serverName, PORT);
+			System.out.println("Connected: " + socket);
+			start();
+		} catch (Exception e) {
+			System.out.println(badHandhake);
+		}
+
+		
+	}
+
+	public Socket getSocket() {
+		return socket;
+	}
+
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+
+	public DataInputStream getStreamIn() {
+		return streamIn;
+	}
+
+	public void setStreamIn(DataInputStream streamIn) {
+		this.streamIn = streamIn;
+	}
+
+	public DataOutputStream getStreamOut() {
+		return streamOut;
+	}
+
+	public void setStreamOut(DataOutputStream streamOut) {
+		this.streamOut = streamOut;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Scanner getClientScanner() {
+		return clientScanner;
+	}
+
+	public void setClientScanner(Scanner clientScanner) {
+		this.clientScanner = clientScanner;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+
+	public void start() throws IOException {
+		streamIn = new DataInputStream(System.in);
+		streamOut = new DataOutputStream(socket.getOutputStream());
+	}
+
+	
+	
 }
